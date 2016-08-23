56dcc9c0d5a062106c830f6927dcc98a65f58bbe
Stayko Nedev <zakkgard@gmail.com>
2016-08-23 14:55:54 +0300

    it appears creature_template was modified at some point.. some work on area triggers for rest

---
+++ Services/WCell.RealmServer/AreaTriggers/ATTemplateTavern.cs
@@ -0,0 +1,31 @@
+﻿using WCell.Constants;
+using WCell.Constants.Items;
+using WCell.Constants.World;
+using WCell.RealmServer.Content;
+using WCell.RealmServer.Global;
+using WCell.Util;
+using WCell.Util.Data;
+using WCell.Util.Graphics;
+
+namespace WCell.RealmServer.AreaTriggers
+{
+    [DataHolder]
+    public class ATTemplateTavern : ATTemplate, IDataHolder
+    {
+        public new void FinalizeDataHolder()
+        {
+            var trigger = AreaTriggerMgr.AreaTriggers.Get(Id);
+            if (trigger == null)
+            {
+                ContentMgr.OnInvalidDBData("AreaTriggerEntry {0} (#{1}, Type: {2}) had invalid AreaTrigger-id.", Name, Id, Type);
+                return;
+            }
+            else
+            {
+                trigger.Template = this;
+            }
+
+            Handler = AreaTriggerMgr.GetHandler(Type);
+        }
+    }
+}
